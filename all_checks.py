773e677d691e4830696bec1f92c7404fb9d022e8
Julie Nguyen <julieng0820@gmail.com>
2021-10-18 14:20:25 +1100

    Iterarte over a list of checks and messages to avoid code duplication

--- all_checks.py
+++ all_checks.py
@@ -24,13 +24,14 @@ def check_root_full():
 
 def main():
     """Refector codes to remove duplication and print all errors related"""
-    if check_reboot():
-        print("Pending Reboot.")
-        sys.exit(1)
-    # Check for at least 2 GB and 10% free
-    if check_root_full():
-        print("Root partition full.")
-        sys.exit(1)
+    checks=[
+        (check_reboot,"Pending Reboot"),
+        (check_root_full,"Root partition full")
+    ]
+    for check, msg in checks:
+        if check():
+            print(msg)
+            sys.exit(1)
 
     print("Everything ok.")
     sys.exit(0)
