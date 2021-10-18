aa22a421d37148b7e1d4dad7dbf4013384678306
Julie Nguyen <julieng0820@gmail.com>
2021-10-18 16:59:09 +1100

    3updating check_no_network function in all_checks.py

--- all_checks.py
+++ all_checks.py
@@ -3,7 +3,6 @@ import os
 import shutil
 import sys
 import socket
-import psutil
 
 def check_reboot():
     """Returns True if the computer has a pending reboot"""
@@ -24,16 +23,20 @@ def check_root_full():
     """Returns True if the root partition is full, False otherwise."""
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
-def check_cpu_constrained():
-    """Returns True if the cpu is having too much usage, False otherwise"""
-    return psutil.cpu_percent(1) > 75
+def check_no_network():
+    """Returns True if it fails to resolve Google's URL, False otherwise"""
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
 
 def main():
     """Refector codes to remove duplication and print all errors related"""
     checks=[
         (check_reboot,"Pending Reboot"),
         (check_root_full,"Root partition full"),
-        (check_cpu_constrained, "CPU is having too much usage")
+        (check_no_network, "No working network.")
     ]
     everything_ok = True
     for check, msg in checks:
